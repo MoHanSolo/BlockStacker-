6b44fa4d4c0525133917d2db3d68333d9880eadd
MoHanSolo <bmohan2795@gmail.com>
2023-05-01 20:36:04 -0500

    The start of something beautiful. I started BlockStacker! I added a README file, a header tag, h2 tag, some slight CSS styling to those tags, as well as a STOP! button.

---
+++ js/main.js
@@ -0,0 +1,29 @@
+alert('js is loaded!')
+
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+        // (model) //
+
+
+
+
+
+/*----- cached elements  -----*/
+        // (view) //
+
+
+
+
+
+/*----- event listeners -----*/
+        // (view) //
+
+
+
+
+        
+/*----- functions -----*/
+    // (controller) //
+
